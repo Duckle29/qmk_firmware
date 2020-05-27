2c31cf86741d717b8494aaa8ba64286296cd43b5
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-05-27 23:39:58 +0200

    Added interrobang

--- keyboards/infinity60/keymaps/duckle/keymap.c
+++ keyboards/infinity60/keymaps/duckle/keymap.c
@@ -22,7 +22,8 @@ enum custom_keycodes {
   UC_SHRG,
   UC_DISA,
   UC_LENN,
-  UC_FACE
+  UC_FACE,
+  UC_BANG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN]   = LAYOUT_60_ansi_split_bs_rshift(
     // Layer 1: General second layer with common functions
-    /*DELETE      F1      F2      F3      F4      F5      F6     F7      F8      F9      F10      F11        F12          VOL-       VOL+
-    */UC(0x203D),KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6, KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,    KC_F12,      KC_VOLD,   KC_VOLU, \
+    /*   ‽      F1      F2      F3      F4      F5      F6     F7      F8      F9      F10      F11        F12          VOL-       VOL+
+    */UC_BANG,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6, KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,    KC_F12,      KC_VOLD,   KC_VOLU, \
     /*  CAPS      F11     UP                                                    MUTE    PL/PS  PREV_TRACK NEXT_TRACK              DELETE
     */KC_CAPS,  KC_F11, KC_UP,_______,_______,_______,_______, UCIS_S, _______, KC_MUTE,KC_MPLY, KC_MPRV,   KC_MNXT,               KC_DEL,  \
     /*           LEFT     DOWN    RIGHT                                                                      HOME    END
@@ -114,11 +115,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           send_unicode_string("👁👄👁");
         }
         break;
+    
+    case UC_BANG:
+        if (record->event.pressed) {
+            send_unicode_hex_string("203D");
+        }
+        break;
+
     default:
       return true;
   }
   return false;
 }
-
-      
-    
